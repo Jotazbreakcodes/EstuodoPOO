79ddcc7941e4848abb2f84bb6cf5422a548a3bb2
Jotazbreakcodes <jodetonador@gmail.com>
2025-05-23 12:01:27 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            try
+            {
+                Console.WriteLine(Calculadora.Divisao(5, 0));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine(Calculadora.Soma(5, 5));
+            Console.WriteLine(Calculadora.Multiplicacao(5, 5));
+            Console.WriteLine(Calculadora.Subtracao(5, 5));
+
+        }
+
+
+        public static class Calculadora
+        {
+            public static decimal Soma(decimal a, decimal b)
+            {
+                return a + b;
+
+            }
+
+            public static decimal Subtracao(decimal a, decimal b)
+            {
+                return a - b;
+
+            }
+            public static decimal Multiplicacao(decimal a, decimal b)
+            {
+                return a * b;
+
+            }
+
+            public static decimal Divisao(decimal a, decimal b)
+            {
+
+                if (b == 0)
+                {
+                    throw new ArgumentException("Não é divisivel por 0");
+                }
+
+                return a / b;
+
+            }
+        }
+    }
+}
