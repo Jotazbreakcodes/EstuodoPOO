a5179a733797f7c2f6bdf5006eccb6caedbb1e1e
Jotazbreakcodes <jodetonador@gmail.com>
2025-05-24 17:49:57 -0300

    Alteração e adição de funcionalidades

--- Program.cs
+++ Program.cs
@@ -1,25 +1,37 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 
-namespace MyApp
+namespace EstudoPOO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            decimal numero, numero2;
             Console.Clear();
+            Console.WriteLine("Digite um número");
+            while (!decimal.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.WriteLine("Digite apenas numeros por favor");
+            }
+
+            Console.WriteLine("Digite outro número");
+            while (!decimal.TryParse(Console.ReadLine(), out numero2))
+            {
+                Console.WriteLine("Digite apenas numeros por favor");
+            }
+
             try
             {
-                Console.WriteLine(Calculadora.Divisao(5, 0));
+                Console.WriteLine(Calculadora.Divisao(numero, numero2));
             }
             catch (ArgumentException ex)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine($" Não é possivel dividir por 0 {ex}");
             }
-            Console.WriteLine(Calculadora.Soma(5, 5));
-            Console.WriteLine(Calculadora.Multiplicacao(5, 5));
-            Console.WriteLine(Calculadora.Subtracao(5, 5));
-
+            Console.WriteLine(Calculadora.Soma(numero, numero2));
+            Console.WriteLine(Calculadora.Multiplicacao(numero, numero2));
+            Console.WriteLine(Calculadora.Subtracao(numero, numero2));
         }
 
 
